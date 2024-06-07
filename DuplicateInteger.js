c1feed52adab97f726a77b6020be112aba5d2d71
Joel T. <SLjoel.ts@gmail.com>
2024-06-07 23:05:21 +0100

    add comments

--- DuplicateInteger.js
+++ DuplicateInteger.js
@@ -1,18 +1,30 @@
+/**
+ * 
+ * 
+ *  Duplicate Integer
+ *  Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+ *  Example 1:
+ *  Input: nums = [1, 2, 3, 3    
+ *  Output: true
+ *  Example 2    
+ *  Input: nums = [1, 2, 3, 4    
+ *  Output: false
+ */
 class Solution {
-    /**
-     * @param {number[]} nums
-     * @return {boolean}
-     */
-    hasDuplicate(nums) {
-        const seen = new Set();
-        for (const num of nums) {
-            if (seen.has(num)) {
-                return true;
-            }
-            seen.add(num);
-        }
-        return false;
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    const seen = new Set();
+    for (const num of nums) {
+      if (seen.has(num)) {
+        return true;
+      }
+      seen.add(num);
     }
+    return false;
+  }
 }
 
 // Example Usage:
--- IsAnagram.js
+++ IsAnagram.js
@@ -1,34 +1,49 @@
+/**
+ *
+ * Is Anagram
+ * Solved
+ * Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ * Example 1:
+ * Input: s = "racecar", t = "carrace"
+ * Output: true
+ * Example 2:
+ * Input: s = "jar", t = "jam"
+ * Output: false
+ * Constraints:
+ * s and t consist of lowercase English letters.
+ */
 class Solution {
-    /**
+  /**
      * @param {string} s
      * @param {string} t
      * @return {boolean}
      */
-    isAnagram(s, t) {
-        if (s.length !== t.length) {
-            return false;
-        }
-        
-        const charCount = {};
-
-        // Count characters in the first string
-        for (let char of s) {
-            charCount[char] = (charCount[char] || 0) + 1;
-        }
-
-        // Subtract counts using the second string
-        for (let char of t) {
-            if (!charCount[char]) {
-                return false;
-            }
-            charCount[char]--;
-            if (charCount[char] < 0) {
-                return false;
-            }
-        }
-
-        return true;
+  isAnagram(s, t) {
+    if (s.length !== t.length) {
+      return false;
     }
+
+    const charCount = {};
+
+    // Count characters in the first string
+    for (let char of s) {
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    // Subtract counts using the second string
+    for (let char of t) {
+      if (!charCount[char]) {
+        return false;
+      }
+      charCount[char]--;
+      if (charCount[char] < 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 // Example Usage:
